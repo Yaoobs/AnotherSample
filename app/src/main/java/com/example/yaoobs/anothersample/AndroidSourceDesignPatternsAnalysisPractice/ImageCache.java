823e3ce5c06f7b49693a1acd490a589e6f3d4670
Yaoobs <yaozheng1990517@163.com>
2017-09-06 19:31:00 +0800

    里氏替换原则

--- app/src/main/java/com/example/yaoobs/anothersample/AndroidSourceDesignPatternsAnalysisPractice/ImageCache.java
+++ app/src/main/java/com/example/yaoobs/anothersample/AndroidSourceDesignPatternsAnalysisPractice/ImageCache.java
@@ -10,3 +10,57 @@ public interface ImageCache {
     public Bitmap get(String url);
     public void put(String url, Bitmap bmp);
 }
+
+////窗口类
+//public class Window {
+//    public void show(View child){
+//        child.draw();
+//    }
+//}
+//
+////建立视图抽象，测量视图的宽高为公用代码，绘制实现交给具体的子类
+//public abstract class View {
+//    public abstract void draw();
+//    public void measure(int width,int height) {
+//        //测量视图大小
+//    }
+//}
+//
+////按钮类的具体实现
+//public class Button extends View {
+//    public void draw(){
+//        //绘制按钮
+//    }
+//}
+////TextView的具体实现
+//public class TextView extends View {
+//    public void draw(){
+//        //绘制文本
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
